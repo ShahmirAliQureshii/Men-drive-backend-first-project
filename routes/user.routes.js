10cd7766385441557b9faecaaeac14dcade0eca6
ShahmirAliQureshii <shahmirali385@gmail.com>
2025-09-07 17:41:27 +0500

    Drive Men Project

---
+++ routes/user.routes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const userModel = require('../models/user');
+const bcrypt = require("bcrypt");
+
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
+router.post('/register',
+    body('email').trim().isEmail().isLength({min: 13}),
+    body('password').trim().isLength({min: 5}),
+    body('username').trim().isLength({min: 3}),
+    async (req, res)=>{
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array(),
+            message: "Invalid Data",
+        })
+    }
+    
+        const {username, email, password} = req.body;
+        const hashPassword = await bcrypt.hash(password, 10);
+
+
+        const newUser = await userModel.create({username, email, password: hashPassword});
+        res.json(newUser)
+})
+
+module.exports = router;
