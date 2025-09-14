41e65b0bd89dea33c40823014ef074b3897d4253
ShahmirAliQureshii <shahmirali385@gmail.com>
2025-09-14 15:03:37 +0500

    Updated Mongo URI for men-drive DB

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-function connectToDB () {
-    mongoose.connect(process.env.MONGO_URI).then(()=>{
-        console.log("Connected to MongoDB");
-    })
+function connectToDB() {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("✅ MongoDB connected"))
+    .catch((err) => console.error("❌ MongoDB error:", err));
 }
-
 
 module.exports = connectToDB;
